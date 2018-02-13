708c84bf01cfe789ff40306df089b2004db8cac5
Romain Calamier <romain.calamier@gmail.com>
2018-02-13 17:49:13 +0100

    get products

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const server = require("./server.js")
+const server = require('./server.js')
 
 server.listen(3002, () => {
-    console.log("Server listening on port 3002")
-});
-
+  console.log('Server listening on port 3002')
+})
+/*  const options =
+        {
+            host: 'localhost',
+            port: 3002,
+            path: '/'
+        }
+    const req = http.get(options, function(res)
+    {
+        console.log("Got response: " + res.statusCode);
+        res.on('data', function(chunk)
+        {
+            page = page + chunk;
+        });
+        server.listen('/products', () => {
+    console.log('test')
+  }) */
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200);
-    res.end();
+  res.writeHead(200)
+
+  if (req.url === '/products' && req.method === 'GET') {
+    const products = JSON.parse(fs.readFileSync('products.json'))
+    for (let i in products) {
+      res.write(products[i].name + ' ' + products[i].price + ' ' + products[i].price)
+    }
+  } else {
+    res.write('Hello world')
+  }
+  res.end()
 })
 
 module.exports = server
