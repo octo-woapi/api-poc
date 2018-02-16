6c9b2f13ad5c8b9e8bc4dfc0a72ef852cc050d69
Romain Calamier <romain.calamier@gmail.com>
2018-02-16 15:50:31 +0100

    post products, incoming: test + modularité/refactoring

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const http = require('http')
 const fs = require('fs')
 
+const productsJSON = JSON.parse(fs.readFileSync('products.json'))
+const products = productsJSON.products
+
 const server = http.createServer((req, res) => {
   res.writeHead(200)
 
   if (req.url === '/products' && req.method === 'GET') {
-    const products = JSON.parse(fs.readFileSync('products.json'))
-    for (let i in products) {
-      res.write(products[i].name + ' ' + products[i].price + ' ' + products[i].price)
-    }
+    console.log(products)
+  } else if (req.url === '/products' && req.method === 'POST') {
+    req.on('data', (chunk) => {
+      const json = JSON.parse(chunk.toString('utf8')) // Need to check JSON format
+      // add the product to the JSON
+      let id = products.length + 1
+      let newproduct = { // conditions
+        'id': id,
+        'name': json.name,
+        'price': json.price,
+        'weight': json.weight
+      }
+      products[id - 1] = newproduct
+      console.log(products)
+      fs.writeFile('products.JSON', "{'products': " + products + '}', (err) => {
+        if (err) throw err
+        console.log('Product has been added')
+      })
+    })
   } else {
     res.write('Hello world')
   }
