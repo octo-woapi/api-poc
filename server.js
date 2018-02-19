60ed49e29c390346695d614d8b59659ae7a7f550
Romain Calamier <romain.calamier@gmail.com>
2018-02-19 18:13:43 +0100

    Test et modularité to be continued, next: create products.js and resolve write product.js error (with more test)

--- server.js
+++ server.js
@@ -1,31 +1,22 @@
 const http = require('http')
-const fs = require('fs')
-
-const productsJSON = JSON.parse(fs.readFileSync('products.json'))
-const products = productsJSON.products
+const tools = require('./tools')
 
 const server = http.createServer((req, res) => {
   res.writeHead(200)
 
   if (req.url === '/products' && req.method === 'GET') {
-    console.log(products)
+    console.log(tools.getProducts())
   } else if (req.url === '/products' && req.method === 'POST') {
     req.on('data', (chunk) => {
-      const json = JSON.parse(chunk.toString('utf8')) // Need to check JSON format
-      // add the product to the JSON
-      let id = products.length + 1
-      let newproduct = { // conditions
-        'id': id,
-        'name': json.name,
-        'price': json.price,
-        'weight': json.weight
+      const json = tools.getPostValues(chunk.toString('utf8'))
+      console.log(json.name + ' ' + json.price + ' ' + json.weight)
+      if (typeof json.name === 'undefined' || !json.name) {
+        throw new Error('Name of the product is undefined')
+      } else {
+        if (typeof json.price === 'undefined' || !json.price) { json.price = 0 }
+        if (typeof json.weight === 'undefined' || !json.weight) { json.weight = 0 }
+        tools.addProduct(json.name, parseFloat(json.price), parseFloat(json.weight))
       }
-      products[id - 1] = newproduct
-      console.log(products)
-      fs.writeFile('products.JSON', "{'products': " + products + '}', (err) => {
-        if (err) throw err
-        console.log('Product has been added')
-      })
     })
   } else {
     res.write('Hello world')
--- test/products.test.js
+++ test/products.test.js
@@ -1,5 +1,6 @@
 const request = require('request')
 const server = require('../server.js')
+const tools = require('../tools.js')
 
 const PORT = 3002
 
@@ -18,5 +19,30 @@ describe('GET /products', () => {
       expect(res.statusCode).toBe(200)
       done()
     })
+  })
+})
+
+describe('POST /products', () => {
+  test('Add data received in the JSON products', (done) => {
+    request.post({url: `http://localhost:${PORT}/products`, form: {'name': 'vanilla', 'price': 10, 'weight': 0.01}}, (err) => {
+      if (err) throw err
+      expect(tools.getProducts()[tools.productsLength]).toEqual({'id': tools.productsLength + 1, 'name': 'vanilla', 'price': 10, 'weight': 0.01})
+      done()
+    })
+  })
+})
+
+describe('Catch missing arguments POST data', () => {
+  test('Catch missing arguments name in POST data', (done) => {
+    let testForm = {'price': 10, 'weight': 0.01};
+    request.post({url: `http://localhost:${PORT}/products`, form: testForm}, (err) => {
+      expect(tools.addProduct(testForm)).toThrow('Name of the product is undefined')
+    })
+  })
+})
+
+describe('Check that error handling works correctly', () => {
+  test('Check that error handling works correctly', () => {
+    expect(tools.connerieThrowException).toThrow(new Error('test Exception'))
   })
 })
